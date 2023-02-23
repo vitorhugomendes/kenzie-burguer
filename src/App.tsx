e9987bd4160224e2640eca29a8f45db236aecbe0
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-02-23 17:42:54 -0300

    fix: toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import Router from './routes';
 import { GlobalStyles } from './styles/global';
 
 const App = () => (
   <>
+    <ToastContainer position='top-center' autoClose={2000} />
     <GlobalStyles />
-    <ToastContainer />
     <Router />
   </>
 );
